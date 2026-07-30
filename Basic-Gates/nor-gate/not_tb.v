5a41878ee2d0516eb9989e904ca06d49ab23124f
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-07-30 12:55:52 +0530

    Add testbench for NOR gate functionality

---
+++ Basic-Gates/nor-gate/not_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+module tb_nor_gate;
+
+reg a;
+reg b;
+wire y;
+
+nor_gate dut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    $dumpfile("nor_gate.vcd");
+    $dumpvars(0, tb_nor_gate);
+
+    a = 0; b = 0; #10;
+    a = 0; b = 1; #10;
+    a = 1; b = 0; #10;
+    a = 1; b = 1; #10;
+
+    $finish;
+end
+
+endmodule
