90de585dd71a9ed2c31da168cf2a3736673b0e4d
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-07-30 16:10:54 +0530

    Reduce delay in testbench for NOR gate

--- Basic-Gates/nor-gate/not_tb.v
+++ Basic-Gates/nor-gate/not_tb.v
@@ -16,10 +16,10 @@ initial begin
     $dumpfile("nor_gate.vcd");
     $dumpvars(0, tb_nor_gate);
 
-    a = 0; b = 0; #10;
-    a = 0; b = 1; #10;
-    a = 1; b = 0; #10;
-    a = 1; b = 1; #10;
+    a = 0; b = 0; #5;
+    a = 0; b = 1; #5;
+    a = 1; b = 0; #5;
+    a = 1; b = 1; #5;
 
     $finish;
 end
