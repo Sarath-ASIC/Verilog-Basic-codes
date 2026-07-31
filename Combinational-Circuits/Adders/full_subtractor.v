62c2144a9dc1579f7dfbe128236e414f7606c2e2
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-07-31 20:54:18 +0530

    Add full subtractor module in Verilog

---
+++ Combinational-Circuits/Adders/full_subtractor.v
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+
+module full_subtractor(
+    input  a,
+    input  b,
+    input  bin,
+    output diff,
+    output bout
+);
+
+assign diff = a ^ b ^ bin;
+
+assign bout = (~a & b) |
+              (~a & bin) |
+              (b & bin);
+
+endmodule
