d91cc0011c3f01136afecc8e7c0d1890c957ed3e
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-08-01 15:57:47 +0530

    Add 8-to-3 encoder module in Verilog
    Implement 8-to-3 encoder in Verilog with case statement.

---
+++ Combinational-Circuits/Encoders/8to3encoder.v
@@ -0,0 +1,24 @@
+`timescale 1ns/1ps
+
+module encoder_8_to_3( input [7:0] i,
+                       output reg [2:0] y
+                       );
+  // output reg [2:0] , becouse in verilog only variables (reg in verilog and logic and systemverilog) can be declared incide the "always" block
+                       always @(*) begin
+                       case(i)
+                       
+                       8'b00000001: y=3'b000;
+                       8'b00000010: y=3'b001;
+                       8'b00000100: y=3'b010;
+                       8'b00001000: y=3'b011;
+                       8'b00010000: y=3'b100;
+                       8'b00100000: y=3'b101;
+                       8'b01000000: y=3'b110;
+                       8'b10000000: y=3'b111;
+                       default: y=3'b000;
+                       endcase 
+                       end
+                       
+                       endmodule
+                         
+                     
