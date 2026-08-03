043b5e439fb9c8c4dc1a0a98145545cd09a22074
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-08-03 09:14:24 +0530

    Add 3-to-1 multiplexer module

---
+++ Combinational-Circuits/Multiplexers/3to1mux.v
@@ -0,0 +1,18 @@
+module mux3to1 (
+    input  wire i0,
+    input  wire i1,
+    input  wire i2,
+    input  wire [1:0] sel,
+    output reg  y
+);
+
+always @(*) begin
+    case (sel)
+        2'b00: y = i0;
+        2'b01: y = i1;
+        2'b10: y = i2;
+        default: y = 1'b0;
+    endcase
+end
+
+endmodule
