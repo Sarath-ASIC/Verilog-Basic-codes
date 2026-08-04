b33165e613dc7b04b5f0fd098f394aa0ee72ef25
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-08-04 12:29:04 +0530

    Create shiftregister_4bit.v

---
+++ Sequential-Circuits/Shift-Register/shiftregister_4bit.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+
+module register_4bit( input [3:0] d, 
+                      input clk, 
+                      output reg [3:0] q
+                      );
+                      
+                      always @(posedge clk)
+                      begin
+                      
+                      q <= d;
+                      
+                      end 
+                      endmodule 
