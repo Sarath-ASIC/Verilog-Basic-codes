e007040986f922da4715b0152cffbaf6926effc7
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-08-04 18:14:19 +0530

    Create siso.v

---
+++ Sequential-Circuits/Shift-Register/siso.v
@@ -0,0 +1,18 @@
+`timescale 1ns/1ps
+
+module siso_sr( input s_in, clk,
+                 output s_out
+                 );
+                 
+                 reg[3:0] shift_register; //shift register bank creation
+                 
+                 assign s_out = shift_register[3];
+                 
+                 always @(posedge clk) 
+                 begin
+                 
+                 shift_register <= {shift_register[2:0], s_in};
+                 
+                 end
+                 
+                 endmodule
