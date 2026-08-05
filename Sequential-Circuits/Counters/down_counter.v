bf11cf63735e591b0eeea7fdc5625c7f1bead44a
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-08-05 21:07:01 +0530

    Create down_counter.v

---
+++ Sequential-Circuits/Counters/down_counter.v
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+module sync_down_counter(
+
+    input clk,
+    input rst,
+
+    output reg [3:0] q
+
+);
+
+always @(posedge clk or posedge rst)
+begin
+
+    if(rst)
+        q <= 4'b1111;      // Start from 15
+
+    else
+        q <= q - 1'b1;
+
+end
+
+endmodule
