9aaf57ec8f3b11959917126ba2d2a06300d6b1a3
ICDesign_Sarath <sarathkcofficial@gmail.com>
2026-08-05 21:02:41 +0530

    Create up_counter.v

---
+++ Sequential-Circuits/Counters/up_counter.v
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+module up_counter (
+
+    input clk,
+    input rst,
+
+    output reg [3:0] q
+
+);
+
+always @(posedge clk or posedge rst)
+begin
+
+    if(rst)
+        q <= 4'b0000;
+
+    else
+        q <= q + 1'b1;
+
+end
+
+endmodule
